d6f505989a1c3475f1c166fde80e151188acadca
HomePC <coxe2754@gmail.com>
2025-03-24 22:22:30 +0900

    인터페이스 추가

--- Source/Project_PZR/Public/KJW/KVRObjectInterface.h
+++ Source/Project_PZR/Public/KJW/KVRObjectInterface.h
@@ -6,6 +6,28 @@
 #include "UObject/Interface.h"
 #include "KVRObjectInterface.generated.h"
 
+UENUM()
+enum class EVRButton
+{
+	NONE,
+	
+	Right_A_Button,
+	Right_B_Button,
+	Left_X_Button,
+	Left_Y_Button,
+
+
+
+	Hand_ThumbUp_Right,
+	Hand_ThumbUp_Left,
+	Hand_Point_Right,
+	Hand_Point_Left,
+	Hand_Trigger_Right,
+	Hand_Trigger_Left,
+
+};
+
+
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class UKVRObjectInterface : public UInterface
@@ -26,5 +48,18 @@ public:
 	void virtual StartGrab(AActor* HandActor) {};
 	void virtual StopGrab(AActor* HandActor) {};
 	void virtual RotObject(const FRotator AddRotator) {};
+
+	//손으로 오브젝트 잡기 시작
+	void virtual StartGrab(class UMotionControllerComponent* MontionComp, bool IsRight) {};
+	//손으로 오브젝트 잡기 종료
+	void virtual StopGrab(class UMotionControllerComponent* MontionComp, bool IsRight) {};
+	//컨트롤러를 통해 오브젝트 회전
+	void virtual RotObject(class UMotionControllerComponent* MontionComp , bool IsRight , const FRotator AddRotator) {};
+
+	//VR버튼이 눌렀을 때 호출
+	void virtual OnButtonPressed(EVRButton VRButton) {};
+	//VR버튼이 떼어졌을 때 호출
+	void virtual OnButtonReleased(EVRButton VRButton) {};
+
 
 };
