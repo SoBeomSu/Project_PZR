0a2a9dd0a65abd94d689691c327d0d14c8365cfd
SeSAcPc_dkwl635 <dkwl635@naver.com>
2025-03-31 13:47:57 +0900

    cs 수정

--- Source/Project_PZR/Project_PZR.Build.cs
+++ Source/Project_PZR/Project_PZR.Build.cs
@@ -8,9 +8,10 @@ public class Project_PZR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","EnhancedInput" , "Niagara" , "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","EnhancedInput" , "Niagara" , "HeadMountedDisplay" , "XRBase" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
